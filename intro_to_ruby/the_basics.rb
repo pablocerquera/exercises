3b7d297bc7356d6f6d52c1e9274e9f8b7bb26b5d
Winston Wolfe <pabz1038@gmail.com>
2022-12-28 13:05:19 -0700

    the basics exercises

---
+++ intro_to_ruby/the_basics.rb
@@ -0,0 +1,33 @@
+first_name = "mister"
+last_name = "potato head"
+
+puts first_name + ' ' + last_name
+
+thou = 5469 / 1000
+hun = 5469 % 1000 / 100
+ten = 5469 % 100 / 10
+ones = 5469 % 10 
+
+puts "thousandths: #{thou}"
+puts "hundredth: #{hun}"
+puts "tenth: #{ten}"
+puts "ones: #{ones}"
+
+movies = {:the_abyss => 1998, :iron_man => 2008, :avatar => 2022}
+
+puts movies[:the_abyss]
+puts movies[:avatar]
+puts movies[:iron_man]
+
+dates = [1998, 2008, 2022]
+puts dates
+
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6* 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6* 5 * 4 * 3 * 2 * 1
+
+puts 5.982 ** 2
+puts 4.674 ** 2
+puts 3290.378 ** 2
