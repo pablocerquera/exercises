de1721d1581f67a81fe34543c400b78fadd5b1aa
Winston Wolfe <pabz1038@gmail.com>
2022-12-28 14:37:52 -0700

    me exercises from the variable chapter

---
+++ intro_to_ruby/name.rb
@@ -0,0 +1,9 @@
+puts "please give me your first name."
+
+first_name = gets.chomp
+puts "please give me your last name"
+last_name = gets.chomp
+
+puts "hello #{first_name} #{last_name}!"
+
+#10.times {puts name}
