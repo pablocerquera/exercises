92dbf27325f7c4dba4ef633d802ce3231e24d804
Winston Wolfe <pabz1038@gmail.com>
2022-12-29 12:29:58 -0700

    the exercise for the method chapter.

---
+++ intro_to_ruby/return.rb
@@ -0,0 +1,22 @@
+=begin def add_three(number)
+  new_value = number + 3
+    puts new_value
+    new_value
+end
+
+add_three(5)
+=end
+def add(a, b)
+   a + b
+end
+def subtract(a, b)
+  a - b
+end
+
+def multiply(num1, num2)
+   num1 * num2
+end
+
+
+add(subtract(80, 10), multiply(add(30, 5), subtract(20, 6)))
+
